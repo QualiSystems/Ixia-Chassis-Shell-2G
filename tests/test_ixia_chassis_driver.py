dbb9295e2c90d1a5fc8002c0ddd1dcef073c38c6
Yoram Shamir <yoram-s@qualisystems.com>
2019-10-30 15:17:08 +0200

    wip - not final, yet

--- tests/test_ixia_chassis_driver.py
+++ tests/test_ixia_chassis_driver.py
@@ -1,30 +1,33 @@
 
 """
-Tests for `TestCenterChassisDriver`
+Tests for `IxiaChassis2GDriver`
 """
 
 import sys
 import logging
 
-from shellfoundry.releasetools.test_helper import create_autoload_context_2g
+from shellfoundry.releasetools.test_helper import create_autoload_context_2g, create_session_from_deployment
 
 from src.driver import IxiaChassis2GDriver
 
-controller = 'localhost'
-port = ''
-address = '192.168.42.61'
-address = '192.168.42.217'
-address = 'localhost'
+controller = ''
+port = '8022'
+address = '192.168.65.32'
+
+attributes = {'Ixia Chassis Shell 2G.Controller Address': controller,
+              'Ixia Chassis Shell 2G.Controller TCP Port': port,
+              'Ixia Chassis Shell 2G.User': 'admin',
+              'Ixia Chassis Shell 2G.Password': 'DxTbqlSgAVPmrDLlHvJrsA=='}
 
 
 class TestIxiaChassis2GDriver(object):
 
     def setup(self):
-        self.context = create_autoload_context_2g(model='Ixia Chassis Shell 2G', address=address,
-                                                  controller=controller, port=port, client_install_path='')
+        self.session = create_session_from_deployment()
+        self.context = create_autoload_context_2g(self.session, 'Ixia Chassis Shell 2G', address, attributes)
         self.driver = IxiaChassis2GDriver()
         self.driver.initialize(self.context)
-        print self.driver.logger.handlers[0].baseFilename
+        print(self.driver.logger.handlers[0].baseFilename)
         self.driver.logger.addHandler(logging.StreamHandler(sys.stdout))
 
     def teardown(self):
@@ -34,8 +37,10 @@ class TestIxiaChassis2GDriver(object):
         pass
 
     def test_autoload(self):
-        self.inventory = self.driver.get_inventory(self.context)
-        for r in self.inventory.resources:
-            print r.relative_address, r.model, r.name
-        for a in self.inventory.attributes:
-            print a.relative_address, a.attribute_name, a.attribute_value
+        inventory = self.driver.get_inventory(self.context)
+        print('\n')
+        for r in inventory.resources:
+            print('{}, {}, {}'.format(r.relative_address, r.model, r.name))
+        print('\n')
+        for a in inventory.attributes:
+            print('{}, {}, {}'.format(a.relative_address, a.attribute_name, a.attribute_value))
--- tests/test_ixia_chassis_shell.py
+++ tests/test_ixia_chassis_shell.py
@@ -1,32 +1,26 @@
 
 """
-Tests for `IxiaChassisDriver`
+Tests for `IxiaChassis2GDriver`
 """
 
-from cloudshell.api.cloudshell_api import ResourceAttributesUpdateRequest, AttributeNameValue, CloudShellAPISession
+import pytest
 
-ixia_chassis = {
-                'win-ixos': {'address': '192.168.42.61',
-                             'controller': '',
-                             'port': '',
-                             'modules': 4,
-                             },
-                'lin-ixos': {'address': '192.168.42.217',
-                             'controller': '',
-                             'port': '8022',
-                             'modules': 1,
-                             },
-                'win-ixos-no-defaults': {'address': '192.168.42.61',
-                                         'controller': '192.168.42.61',
-                                         'port': '4555',
-                                         'modules': 4,
-                                         },
-                'lin-ixos-from-ixtcl-server': {'address': '192.168.42.217',
-                                               'controller': 'localhost',
-                                               'port': '',
-                                               'modules': 1,
-                                               },
-                }
+from cloudshell.api.cloudshell_api import AttributeNameValue
+
+from shellfoundry.releasetools.test_helper import create_session_from_deployment, create_autoload_resource_2g
+
+chassis_properties = {'win-ixos': {'address': '192.168.42.61',
+                                   'controller': '',
+                                   'port': '',
+                                   'modules': 4},
+                      'lin-ixos': {'address': '192.168.65.32',
+                                   'controller': '',
+                                   'port': '8022',
+                                   'modules': 1},
+                      'lin-ixos-from-ixtcl-server': {'address': '192.168.65.32',
+                                                     'controller': 'localhost',
+                                                     'port': '',
+                                                     'modules': 1}}
 
 
 class TestIxiaChassis2GShell(object):
@@ -34,39 +28,21 @@ class TestIxiaChassis2GShell(object):
     session = None
 
     def setup(self):
-        self.session = CloudShellAPISession('localhost', 'admin', 'admin', 'Global')
+        self.session = create_session_from_deployment()
 
     def teardown(self):
-        for resource in self.session.GetResourceList('Testing').Resources:
-            self.session.DeleteResource(resource.Name)
+        self.session.DeleteResource(self.resource.Name)
 
     def test_hello_world(self):
         pass
 
-    def test_win_ixos(self):
-        self._get_inventory('win-ixos', ixia_chassis['win-ixos'])
-
-    def test_lin_ixos(self):
-        self._get_inventory('lin-ixos', ixia_chassis['lin-ixos'])
-
-    def test_all(self):
-        for key, value in ixia_chassis.items():
-            self._get_inventory(key, value)
-
-    def _get_inventory(self, name, properties):
-        self.resource = self.session.CreateResource(resourceFamily='CS_TrafficGeneratorChassis',
-                                                    resourceModel='Ixia Chassis Shell 2G',
-                                                    resourceName=name,
-                                                    resourceAddress=properties['address'],
-                                                    folderFullPath='Testing',
-                                                    parentResourceFullPath='',
-                                                    resourceDescription='should be removed after test')
-        self.session.UpdateResourceDriver(self.resource.Name, 'Ixia Chassis Shell 2G')
-        attributes = [AttributeNameValue('Ixia Chassis Shell 2G.Controller Address', properties['controller']),
-                      AttributeNameValue('Ixia Chassis Shell 2G.Controller TCP Port', properties['port']),
-                      AttributeNameValue('Ixia Chassis Shell 2G.Client Install Path', '')]
-        self.session.SetAttributesValues(ResourceAttributesUpdateRequest(self.resource.Name, attributes))
+    @pytest.mark.parametrize('chassis', ['win-ixos', 'lin-ixos-from-ixtcl-server'])
+    def test_autoload(self, chassis):
+        attributes = [
+            AttributeNameValue('Ixia Chassis Shell 2G.Controller Address', chassis_properties[chassis]['controller']),
+            AttributeNameValue('Ixia Chassis Shell 2G.Controller TCP Port', chassis_properties[chassis]['port'])]
+        self.resource = create_autoload_resource_2g(self.session, 'Ixia Chassis Shell 2G',
+                                                    chassis_properties[chassis]['address'], chassis, attributes)
         self.session.AutoLoad(self.resource.Name)
         resource_details = self.session.GetResourceDetails(self.resource.Name)
-        assert(len(resource_details.ChildResources) == properties['modules'])
-        self.session.DeleteResource(self.resource.Name)
+        assert(len(resource_details.ChildResources) == chassis_properties[chassis]['modules'])
